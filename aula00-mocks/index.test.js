57421687b318efda6969a6f2a08df41508282840
victoriacassolo <vitoria.cassolo@gmail.com>
2023-03-24 13:48:41 -0300

    feat: finish tests mocks!

--- aula00-mocks/index.test.js
+++ aula00-mocks/index.test.js
@@ -11,4 +11,29 @@ const assert = require('assert');
     const result = File.csvToJSON(filePath);
     await assert.rejects(result, expected);
   }
+
+  {
+    const filePath = './mocks/invalid-header.csv';
+    const expected = new Error(error.FILE_FIELDS_ERROR_MESSAGE);
+    const result = File.csvToJSON(filePath);
+    await assert.rejects(result, expected);
+  }
+
+  {
+    const filePath = './mocks/fiveItems-invalid.csv';
+    const expected = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
+    const result = File.csvToJSON(filePath);
+    await assert.rejects(result, expected);
+  }
+
+  {
+    const filePath = './mocks/threeItems-valid.csv';
+    const expected = [
+      { id: 1, name: 'xuxa da silva', profession: 'developer', age: 120 },
+      { id: 2, name: 'jose da silva', profession: 'developer', age: 60 },
+      { id: 3, name: 'zezin', profession: 'QA', age: 25 },
+    ];
+    const result = await File.csvToJSON(filePath);
+    assert.deepEqual(result, expected);
+  }
 })();
--- aula00-mocks/src/file.js
+++ aula00-mocks/src/file.js
@@ -1,4 +1,5 @@
 const { readFile } = require('fs/promises');
+const { error } = require('./constants');
 const DEFAULT_OPTION = {
   maxLines: 3,
   fields: ['id', 'name', 'profession', 'age'],
@@ -10,6 +11,9 @@ class File {
     const validation = this.isValid(content);
 
     if (!validation.valid) throw new Error(validation.error);
+
+    const result = this.parseCSVToJSON(content);
+    return result;
   }
 
   static isValid(csvString, options = DEFAULT_OPTION) {
@@ -21,14 +25,43 @@ class File {
     //[2] = linha 2
     // ...variavel = restante do arquivo
 
-    const [headers, ...fileWithoutHeader] = csvString.split(/\r?\n/);
-    if (!fileWithoutHeader.length) {
+    const [header, ...fileWithoutHeader] = csvString.split(/\r?\n/);
+    const isHeaderValid = header === options.fields.join(',');
+
+    if (!isHeaderValid) {
+      return {
+        error: error.FILE_FIELDS_ERROR_MESSAGE,
+        valid: false,
+      };
+    }
+    if (
+      !fileWithoutHeader.length ||
+      fileWithoutHeader.length > options.maxLines
+    ) {
       return {
         error: error.FILE_LENGTH_ERROR_MESSAGE,
         valid: false,
       };
     }
-    console.log(headers, fileWithoutHeader);
+
+    return { valid: true };
+  }
+
+  static parseCSVToJSON(csvString) {
+    const lines = csvString.split(/\r?\n/);
+    // remover a primeira linha (header)
+    const firstLine = lines.shift();
+    const header = firstLine.split(',');
+
+    const users = lines.map((line) => {
+      const columns = line.split(',');
+      const user = {};
+      for (const index in columns) {
+        user[header[index]] = columns[index].trim();
+      }
+      return user;
+    });
+    return users;
   }
 }
 module.exports = File;
